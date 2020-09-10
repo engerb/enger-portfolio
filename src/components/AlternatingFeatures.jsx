580aeee0189cdf4d4611281d6f66346fe7706dc7
engerb <engerbewza@gmail.com>
2020-09-09 21:53:30 -0700

    finished alternating features section

--- src/components/AlternatingFeatures.jsx
+++ src/components/AlternatingFeatures.jsx
@@ -23,12 +23,12 @@ export default (props) => {
                 {props.features.map((feature, indexKey) => {
                     return <div key={indexKey} className={`demoMain ${feature.class}`} style={{minHeight: feature.demo.height}}>
                         <div className={`demo`} style={{width: feature.demo.width, height: feature.demo.height}}>
-                            {feature.overlayImage && <div className={`overlayImage ${feature.overlayImage.class}`} style={{backgroundImage: 'url(' + feature.overlayImage.img + ')'}} />}
                             {feature.mask && <feature.mask askID={'mask' + indexKey} key={'mask' + indexKey} />}
                             <div className={`shadow`} style={{width: feature.demo.width, height: feature.demo.height, borderRadius: feature.demo.radius, clipPath: 'url(#mask' + indexKey + ')'}} />
                             <div className={`img`} style={{width: feature.demo.width, height: feature.demo.height, backgroundImage: 'url(' + feature.demo.img + ')', borderRadius: feature.demo.radius, clipPath: 'url(#mask' + indexKey + ')'}}>
                                 {feature.demo.video && <div></div>}
                             </div>
+                            {feature.overlayImage && <div className={`overlayImage ${feature.overlayImage.class}`} style={{backgroundImage: 'url(' + feature.overlayImage.img + ')'}} />}
                         </div>
                         <div className={`content`} style={{minHeight: feature.demo.height}}>
                             <div className={`text`}>
